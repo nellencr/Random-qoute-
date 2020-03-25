563aff5d621167fbd4564d893f728af2d6c56382
nela <nellencr@gmail.com>
2020-03-25 14:49:10 +0200

    random quote machine created using html and scc style to imprve it and js to make it funtion.

---
+++ main.js
@@ -0,0 +1,25 @@
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+function getQuote() {
+    fetch(endpoint)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayQuote(data.message);
+        })
+        .catch(function() {
+            console.log("An error occurred");
+        });
+}
+
+function displayQuote(quote) {
+    const quoteText = document.querySelector('.quote-text');
+    quoteText.textContent = quote;
+
+    const tweetButton = document.querySelector('.tweet');
+    tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
+}
+
+const newQuoteButton = document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
